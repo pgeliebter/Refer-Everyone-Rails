f76888c5e0a97ec5ed8b51afca5dda5c88feae99
Pinny Geliebter <pgibralter@gmail.com>
2021-07-28 15:41:38 -0400

    adds campaign route shells and adds authenticate_user to campaign controller and fills in campaign index

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,2 +1,14 @@
 class CampaignsController < ApplicationController
+  before_action :authenticate_user
+
+  def index
+    campaigns = current_user.campaigns
+    render json: campaigns
+  end
+
+  def show
+  end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   post "users" => "users#create"
-  post "/sessions" => "sessions#create"
+  post "sessions" => "sessions#create"
+
+  get "campaigns" => "campaigns#index"
+  get "campaings/:id" => "campaigns#show"
+  post "campaigns" => "campaigns#create"
 end
