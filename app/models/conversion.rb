26134c5c4195441d1ebd4fad66d9b3a70f37917d
Pinny Geliebter <pgibralter@gmail.com>
2021-07-28 14:18:27 -0400

    adds self-join for conversions

--- app/models/conversion.rb
+++ app/models/conversion.rb
@@ -1,3 +1,5 @@
 class Conversion < ApplicationRecord
   belongs_to :campaign
+  has_many :referrals, class_name: "Conversion", foreign_key: "referred_by"
+  belongs_to :referred_by, class_name: "Conversion", foreign_key: "referred_by"
 end
