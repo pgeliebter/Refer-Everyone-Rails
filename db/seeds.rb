05431d4c8ddc3c4c763d06b6ad03b7e42865e4fe
Pinny Geliebter <pgibralter@gmail.com>
2021-07-28 13:54:22 -0400

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,11 @@ Campaign.create!([
   { name: "Fly a kite", company: "KiteCo", user_id: 2 },
   { name: "Coders need us!", company: "Github", user_id: 1 },
 ])
-Conversions.create!([
+Conversion.create!([
   { first_name: "Bob", last_name: "Dillion", email: "bob@example.com", phone: "333-333-3333", campaign_id: 1 },
-  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 1, reffered_by_id: 1 },
-  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", phone: "555-555-5555", campaign_id: 1, reffered_by_id: 1 },
-  { first_name: "Boo", last_name: "Flack", email: "boo@example.com", phone: "666-666-6666", campaign_id: 1, reffered_by_id: 1 },
-  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", phone: "444-444-4444", campaign_id: 1, reffered_by_id: 1 },
+  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 1, reffered_by: 1 },
+  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", phone: "555-555-5555", campaign_id: 1, reffered_by: 1 },
+  { first_name: "Boo", last_name: "Flack", email: "boo@example.com", phone: "666-666-6666", campaign_id: 2 },
+  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 2, reffered_by: 4 },
 
 ])
