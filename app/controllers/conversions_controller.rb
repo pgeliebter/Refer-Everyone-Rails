50485c3ad225de014b8c50dfbbc89c16ffe74a8c
Pinny Geliebter <pgibralter@gmail.com>
2021-07-28 16:30:44 -0400

    adds create method

--- app/controllers/conversions_controller.rb
+++ app/controllers/conversions_controller.rb
@@ -1,2 +1,20 @@
 class ConversionsController < ApplicationController
+  def create
+    conversion = Conversion.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone: params[:phone],
+      campaign_id: params[:campaign_id],
+      referred_by: params[:referred_by],
+    )
+    if conversion.save
+      render json: { message: "Conversion created successfully" }, status: :created
+    else
+      render json: { errors: conversion.errors.full_messages }, status: 422
+    end
+  end
+
+  def show
+  end
 end
