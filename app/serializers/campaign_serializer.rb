868a3a6ce95abefbb4e3e8b2505886a7954f1981
Pinny Geliebter <pgibralter@gmail.com>
2021-08-16 14:24:58 -0400

    updates serializer and creates new user serialzier

--- app/serializers/campaign_serializer.rb
+++ app/serializers/campaign_serializer.rb
@@ -1,6 +1,6 @@
 class CampaignSerializer < ActiveModel::Serializer
   attributes :id, :name, :company, :created_at, :incentive, :total_conversions
-  belongs_to :user
 
+  belongs_to :user
   has_many :conversions, serializer: ConversionSerializer
 end
--- app/serializers/conversion_serializer.rb
+++ app/serializers/conversion_serializer.rb
@@ -1,5 +1,5 @@
 class ConversionSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :email, :phone, :created_at, :total_referrals, :referrals, :referred_by
+  attributes :id, :first_name, :last_name, :email, :phone, :created_at, :total_referrals, :referrals, :referred_by, :total_incentive
   belongs_to :campaign
   belongs_to :referred_by
   has_many :referrals
