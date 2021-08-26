1dd99021a4dd40aac14a02fe549c5be785a27303
Pinny Geliebter <pgibralter@gmail.com>
2021-08-26 16:56:58 -0400

    final commit for V1

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 User.create!([
 
-  { first_name: "Ralph", last_name: "Carlyle", email: "ralph@example.com", password_digest: nil, phone: "111-111-1111" },
-  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", password_digest: nil, phone: "222-222-2222" },
+  { first_name: "Ralph", last_name: "Carlyle", email: "ralph@example.com", password: "password", phone: "111-111-1111" },
+  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", password: "password", phone: "222-222-2222" },
 
 ])
 Campaign.create!([
-  { name: "We all scream for ice cream", company: "Haagen-Dazs", user_id: 1 },
+  { name: "We all scream for ice cream", company: "Haagen-Dazs", user_id: 1, incentive: 43.12 },
 
-  { name: "Fly a kite", company: "KiteCo", user_id: 2 },
-  { name: "Coders need us!", company: "Github", user_id: 1 },
+  { name: "Fly a kite", company: "KiteCo", user_id: 2, incentive: 15.00 },
+  { name: "Coders need us!", company: "Github", user_id: 1, incentive: 15.00 },
 ])
 Conversion.create!([
   { first_name: "Bob", last_name: "Dillion", email: "bob@example.com", phone: "333-333-3333", campaign_id: 1 },
-  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 1, referred_by: 1 },
-  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", phone: "222-222-2222", campaign_id: 1, referred_by: 1 },
+  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 1 },
+  { first_name: "Yaa", last_name: "Geli", email: "yaa@example.com", phone: "222-222-2222", campaign_id: 1 },
   { first_name: "Boo", last_name: "Flack", email: "boo@example.com", phone: "666-666-6666", campaign_id: 2 },
-  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 2, referred_by: 4 },
+  { first_name: "Captain", last_name: "Edwards", email: "captain@example.com", phone: "444-444-4444", campaign_id: 2 },
 
 ])
